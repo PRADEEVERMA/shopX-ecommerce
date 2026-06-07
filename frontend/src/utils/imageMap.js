425704d6eaca0cb1e8dec5e4043e9ff0e1f71fe0
Pradeep Verma <pradeepverma.s2003@gmail.com>
2026-06-07 11:41:23 +0530

    Temporarily remove sunglasses image

--- frontend/src/utils/imageMap.js
+++ frontend/src/utils/imageMap.js
@@ -5,7 +5,7 @@ import headphonesImg from "../assets/images/p-headphones.jpg";
 import earbudsImg from "../assets/images/cat-electronics.jpg";
 import bagImg from "../assets/images/p-bag.jpg";
 import fashionGirlImg from "../assets/images/n-Bags.avif";
-import sunglassesImg from "../assets/images/k-glass.webp";
+import sunglassesImg from "../assets/images/sunglasses.webp";
 import sofaImg from "../assets/images/home-sofa.webp";
 import chairImg from "../assets/images/home-chair.jpg";
 import lampImg from "../assets/images/home-lamp.jpg";
